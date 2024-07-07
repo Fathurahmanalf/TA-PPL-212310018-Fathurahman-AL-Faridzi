9afa8bddc760ba5e2aaac5ec585012f3a5fb25aa
Fathurahmanalf <113759542+Fathurahmanalf@users.noreply.github.com>
2024-07-07 17:45:59 +0700

    TA-PPL-212310018-Fathurahman AL Faridzi
    TA-PPL-212310018-Fathurahman AL Faridzi

---
+++ TA-PPL-212310018-Fathurahman AL Faridzi/TA PPL - Test Script - Fathurahman AL Faridzi - 212310018/org_admin.py
@@ -0,0 +1,64 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+import time
+
+
+@pytest.fixture()
+def driver():
+    driver = webdriver.Chrome()
+    driver.get('https://opensource-demo.orangehrmlive.com/')
+    driver.maximize_window()
+    yield driver
+    driver.quit()
+
+
+def test(driver):
+    driver.find_element(By.NAME, 'username').send_keys('Admin')
+    driver.find_element(By.NAME, 'password').send_keys('admin123' + Keys.ENTER)
+    time.sleep(3)
+
+    # Masuk menu admin
+    driver.find_element(
+        By.XPATH, '//*[@id="app"]/div[1]/div[1]/aside/nav/div[2]/ul/li[1]/a').click()
+    time.sleep(3)
+
+    # Mencari user dengan username yang benar
+    driver.find_element(
+        By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[1]/div/div[2]/input').send_keys('Bow')
+    driver.find_element(
+        By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[2]/button[2]').click()
+    time.sleep(3)
+    driver.find_element(
+        By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[2]/button[1]').click()
+
+    # Mencari user dengan username salah
+    driver.find_element(
+        By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[1]/div/div[2]/input').send_keys('fathur')
+    driver.find_element(
+        By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[3]/div/div[2]/div/div/input').send_keys('fathurahman')
+    driver.find_element(
+        By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[2]/button[2]').click()
+    time.sleep(3)
+
+
+    # Menampilkan_data_User_tanpa_memasukkan/menginput
+    driver.find_element(
+        By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[2]/button[2]').click()
+    time.sleep(3)
+    driver.find_element(
+        By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[2]/button[1]').click()
+
+    # Menambah_User_baru_tanpa_mengisi/menginput
+    driver.find_element(
+        By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div[2]/div[1]/button').click()
+    time.sleep(3)
+    driver.find_element(
+        By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[3]/button[2]').click()
+    time.sleep(3)
+    driver.find_element(
+        By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[3]/button[1]').click()
+    time.sleep(3)
+    
